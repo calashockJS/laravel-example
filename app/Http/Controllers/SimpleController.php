792aeefc66d43d3b951d77dfabb8874160d138d5
Judhisthira Sahoo <judhisahooo@gmail.com>
2025-02-20 11:34:38 +0530

    Adde soem env dat in ctonroller

--- app/Http/Controllers/SimpleController.php
+++ app/Http/Controllers/SimpleController.php
@@ -7,10 +7,12 @@ use Illuminate\Http\Request;
 class SimpleController extends Controller
 {
     public function index(){
+        $appURL = env('APP_URL');
         $faceData = [
             'id' =>1,
             'name' => 'Tesat name',
-            'emal' => 'emailfake@gmail.com'
+            'emal' => 'emailfake@gmail.com',
+            'appURL' => $appURL
         ];
         return response()->json($faceData);
     }
